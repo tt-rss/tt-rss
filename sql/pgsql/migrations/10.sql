c1629a722150add1f4cd400b69698a12321331ea
LaurentGH <31282893+LaurentGH@users.noreply.github.com>
2025-11-04 13:55:59 +0100

    Update 10.sql to replace e-mail by email

--- sql/pgsql/migrations/10.sql
+++ sql/pgsql/migrations/10.sql
@@ -1,5 +1,5 @@
 insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id,help_text) values('DIGEST_ENABLE', 1, 'false', 'Enable email digest',1,
-'This option enables sending daily digest of new (and unread) headlines on your configured e-mail address');
+'This option enables sending daily digest of new (and unread) headlines on your configured email address');
 
 alter table ttrss_feeds add column include_in_digest boolean;
 update ttrss_feeds set include_in_digest = true;
