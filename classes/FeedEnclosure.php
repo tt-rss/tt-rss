0b513302c49fcbfbe3033a9c32ef3bc32c0b70a6
Greg <supahgreg@users.noreply.github.com>
2026-05-31 15:44:45 -0500

    Exclude labels from article hash calculation, improve tag+enclosure change detection (#316)
    * Exclude labels from article hash calculation, properly detect tag+enclosure changes.
    
    * Make Rector happy about 'FeedEnclosure#__toString()'.

--- classes/FeedEnclosure.php
+++ classes/FeedEnclosure.php
@@ -1,9 +1,21 @@
 <?php
-class FeedEnclosure {
+class FeedEnclosure implements \Stringable {
 	public string $link = '';
 	public string $type = '';
 	public string $length = '';
 	public string $title = '';
 	public string $height = '';
 	public string $width = '';
+
+	public function __toString(): string {
+		return sprintf(
+			'FeedEnclosure(link=%s, type=%s, length=%s, title=%s, height=%s, width=%s)',
+			$this->link,
+			$this->type,
+			$this->length,
+			$this->title,
+			$this->height,
+			$this->width,
+		);
+	}
 }
