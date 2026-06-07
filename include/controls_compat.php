tt-rss/tt-rss

commit 2e23c136ae3ce08739ed4a13d65e9726a9f1ca40

Fix caching of theme content.

include/controls_compat.php
@@ -6,7 +6,8 @@
 function stylesheet_tag(string $filename, array $attributes = []): string {
 
 	$attributes_str = \Controls\attributes_to_string([
-		'href' => "$filename?" . filemtime($filename),
+		'href' => $filename
+			. (preg_match('/\?\d+$/', $filename) || !is_file($filename) ? '' : '?' . filemtime($filename)),
 		'rel' => 'stylesheet',
 		'type' => 'text/css',
 		'data-orig-href' => $filename,

include/functions.php
@@ -472,14 +472,14 @@
 		return false;
 	}
 
-	function get_theme_path(string $theme, string $default = ""): string {
-		$check = "themes/$theme";
-		if (file_exists($check)) return $check;
+	function get_theme_path(string $theme, string $default = ''): string {
+		foreach (['themes', Config::get(Config::LOCAL_THEMES_DIR)] as $d) {
+			$f = "$d/$theme";
+			if (is_file($f))
+				return $f . '?' . filemtime($f);
+		}
 
-		$check = Config::get(Config::LOCAL_THEMES_DIR) . "/$theme";
-		if (file_exists($check)) return $check;
-
-		return $default;
+		return $default . (is_file($default) ? '?' . filemtime($default) : '');
 	}
 
 	function theme_exists(string $theme): bool {

js/App.js
@@ -190,7 +190,7 @@ const App = {
 			xhr.post("backend.php", {op: "RPC", method: "getRuntimeInfo"}, () => {
 				const css_override = is_night ? App.getInitParam("default_dark_theme") : App.getInitParam("default_light_theme");
 
-				link.setAttribute("href", css_override + "?" + Date.now());
+				link.setAttribute('href', css_override);
 
 				window.clearTimeout(this._night_mode_retry_timeout);
 			});

js/utility.js
@@ -12,7 +12,7 @@ window.addEventListener("load", function() {
             if (link) {
                 const css_override = is_night ? dark_theme : light_theme;
 
-                link.setAttribute("href", css_override + "?" + Date.now());
+                link.setAttribute('href', css_override);
             }
         },
         setup_night_mode: function() {

commit c04685063d4212a12f649c9876a1f170d4dae91b

Fix an issue with override JS src
Introduced in 2e23c136ae3ce08739ed4a13d65e9726a9f1ca40.

include/controls_compat.php
@@ -22,7 +22,8 @@ function stylesheet_tag(string $filename, array $attributes = []): string {
  */
 function javascript_tag(string $filename, array $attributes = []): string {
 	$attributes_str = \Controls\attributes_to_string([
-		'src' => "$filename?" . filemtime($filename),
+		'src' => $filename
+			. (preg_match('/\?\d+$/', $filename) || !is_file($filename) ? '' : '?' . filemtime($filename))
 		'type' => 'text/javascript',
 		'charset' => 'utf-8',
 		...$attributes,